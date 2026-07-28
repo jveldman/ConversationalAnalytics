87213ae61fdaee9c80cbf97aa1dcfb8cbb4a7113
jveldman <jjh.veldman@outlook.com>
2026-07-28 14:53:06 +0200

    devserver
    added devserver to docker-compose and cube.js. For local files, docker-compose is ignored, so it will default to false. Locally, it is set to true.

--- cube-core/cube.js
+++ cube-core/cube.js
@@ -1,7 +1,7 @@
 const { DuckDBDriver } = require('@cubejs-backend/duckdb-driver');
 
 module.exports = {
-  devServer: true,
+  devServer: process.env.DEVSERVER === 'true',
   driverFactory: () => new DuckDBDriver({
     database: process.env.CUBEJS_DB_DUCKDB_DATABASE_PATH,
   }),
